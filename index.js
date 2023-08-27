98cb8ceca64a7f2aa13534723d9701acbd2523a5
RoDoCode <>
2023-08-27 13:07:12 +0000

    add horizontal win condition and checker

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var currPlayer = playerRed;
 
 var gameOver = false;
 var board;
+var currColumns;
 
 var rows = 6;
 var columns = 7;
@@ -14,6 +15,7 @@ window.onload = function() {
 
 function setGame() {
     board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
 
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c< columns; c++) {
@@ -38,8 +40,13 @@ function setPiece() {
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
 
+    r = currColumns[c];
+    if (r < 0) {
+        return;
+    }
+
     board[r][c] = currPlayer;
-    let tile = this;
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
     if (currPlayer == playerRed) {
         tile.classList.add("red-piece");
         currPLayer = playerYellow;
@@ -49,4 +56,33 @@ function setPiece() {
         currPlayer = playerRed;
     }
 
+    r -= 1; //updating row height for a column
+    currColumns[c] = r; // update board array
+
+    checkWinner();
+}
+
+fucntion checkWinner() {
+    // horizontal win check
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c< columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("winner");
+    if (board[r][c] == playerRed) {
+        winner.innerText = "Red Wins";
+    } else {
+        winner.innerText = "Yellow Wins";
+    }
+
+    gameOver = true;
 }
